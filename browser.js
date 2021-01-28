2874965c3972130cbbcd2970f4a8ead019149328
Fredrik Johansson <jeanfredrik@gmail.com>
2021-01-28 17:03:12 +0100

    feat: export `prependUsedFragments`

--- browser.js
+++ browser.js
@@ -1,1 +1,2 @@
 exports.useGraphQLFragmentPrepender = require("gatsby-util-fragments-extractor/useGraphQLFragmentPrepender");
+exports.prependUsedFragments = require("gatsby-util-fragments-extractor/prependUsedFragments");
